eda87b45eb255fd83c27dd272a80e176fec8eb59
kshitijraj-06 <kshitij6102004@gmail.com>
2024-02-21 09:36:24 +0530

    Started building the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MaterialApp(
@@ -7,37 +11,130 @@ void main() {
   ));
 }
 
-class WeatherApp extends StatelessWidget {
+class WeatherApp extends StatefulWidget {
   WeatherApp({super.key});
+
+  @override
+  State<WeatherApp> createState() => _WeatherAppState();
+}
+
+class _WeatherAppState extends State<WeatherApp> {
+  final String apiKey =
+      '960437de000ee1c797293d478704840f'; // OpenWeatherMap API Key
+  var temperature;
+  var description;
+  var currently;
+  var humidity;
+  var windSpeed;
+
+  Future<void> getWeather(double lat, double lon) async {
+    final url =
+        'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apiKey&units=metric';
+    final response = await http.get(Uri.parse(url));
+    final decoded = json.decode(response.body);
+
+    setState(() {
+      temperature = decoded['main']['temp'];
+      description = decoded['weather'][0]['description'];
+      currently = decoded['weather'][0]['main'];
+      humidity = decoded['main']['humidity'];
+      windSpeed = decoded['wind']['speed'];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationAndWeather();
+  }
+
+  Future<void> getLocationAndWeather() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    double lat = position.latitude;
+    double lon = position.longitude;
+    await getWeather(lat, lon);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.blueGrey,
-        body: Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 60.0, left: 20),
-              child: IconButton(
-                onPressed: () {
-                  //logic
-                },
-                icon: Icon(
-                  Icons.home_outlined,
-                  size: 40,
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 60.0, left: 20),
+                child: IconButton(
+                  onPressed: () {
+                    //logic
+                  },
+                  icon: Icon(
+                    Icons.refresh_sharp,
+                    size: 30,
+                  ),
                 ),
               ),
+              SizedBox(
+                width: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 60.0, left: 20),
+                child: Text(
+                  'Weather App',
+                  style: GoogleFonts.abel(
+                      textStyle: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  )),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 100,
+          ),
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 13.0, left: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Temperature : $temperature °C',
+                    style: GoogleFonts.abel(
+                        textStyle: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w400)),
+                  ),
+                  Text(
+                    'Description : $currently',
+                    textAlign: TextAlign.start,
+                    style: GoogleFonts.abel(
+                        textStyle: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w400)),
+                  ),
+                  Text(
+                    'Humidity : $humidity %',
+                    textAlign: TextAlign.start,
+                    style: GoogleFonts.abel(
+                        textStyle: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w400)),
+                  ),
+                ],
+              ),
             ),
-            SizedBox(width: 80,),
-            Padding(
-              padding: const EdgeInsets.only(top:66.0, left: 20),
-              child: Text('Weather App',
-              style: GoogleFonts.abel(
-                textStyle: TextStyle(fontSize: 20)
-              ),),
+            constraints: BoxConstraints(
+              minHeight: 100,
+              minWidth: 400,
             ),
-          ],
-        )
+            decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                color: Colors.grey[400],
+                borderRadius: BorderRadius.circular(20)),
+          ),
+        ],
+      ),
     );
   }
 }
